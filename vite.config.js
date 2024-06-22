914561bc4f2d05721dc489e6f859e8e1ee6674ad
BasemYahia402 <basemyahia70@gmail.com>
2024-06-22 15:50:43 +0300

    Update file vite.config.js to deploy githup pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/Note-App/", // Replace 'your-repository-name' with your actual repository name
   build: {
     outDir: "dist",
   },
